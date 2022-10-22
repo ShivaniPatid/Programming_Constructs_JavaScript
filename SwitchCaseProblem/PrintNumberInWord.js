bf4bc00c9d33329fd04b1bee88ee1fe5cc14cb56
ShivaniPatid <shivanipatidar712@gmail.com>
2022-10-22 20:10:49 +0530

    [Shivani] print a number in a word using switch case

---
+++ SwitchCaseProblem/PrintNumberInWord.js
@@ -0,0 +1,38 @@
+let number=7;
+let text;
+switch(number)
+{
+    case 0:
+        text="Zero";
+        break;
+    case 1:
+        text="One";
+        break;
+    case 2:
+        text="Two";
+        break;
+    case 3:
+        text="Three";
+        break;
+    case 4:
+        text="Four";
+        break;
+    case 5:
+        text="Five";
+        break;
+    case 6:
+        text="Six";
+        break;
+    case 7:
+        text="Seven";
+        break;
+    case 8:
+        text="Eight";
+        break;
+    case 9:
+        text="Nine";
+    default:
+        text="Whatever";
+
+}
+console.log(text);
