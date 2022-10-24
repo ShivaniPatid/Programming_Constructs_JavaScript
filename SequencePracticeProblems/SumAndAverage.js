6f1dafbafa84b105da956b6f588e26db11848cf2
ShivaniPatid <shivanipatidar712@gmail.com>
2022-10-24 06:53:49 +0530

    [Shivani] read 5 random 2 digit values then find sumand average

---
+++ SequencePracticeProblems/SumAndAverage.js
@@ -0,0 +1,10 @@
+let a = Math.floor(Math.random()*90+10);
+let b = Math.floor(Math.random()*90+10);
+let c = Math.floor(Math.random()*90+10);
+let d = Math.floor(Math.random()*90+10);
+let e = Math.floor(Math.random()*90+10);
+
+let sum=(a+b+c+d+e);
+let avg=sum/5;
+console.log("Sum : "+sum);
+console.log("Avg : "+avg);
