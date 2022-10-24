c2267c30b4eb1a13217d6bb9f6059717b3f3a547
ShivaniPatid <shivanipatidar712@gmail.com>
2022-10-24 07:14:17 +0530

    [Shivani] Unit Conversion

---
+++ SequencePracticeProblems/UnitConversion.js
@@ -0,0 +1,13 @@
+// 1ft = 12 in then 42 in = ? ft
+let inches=42;
+let result1=inches/12;
+console.log("42 in = "+result1);
+
+//Rectangular Plot of 60 feet x 40 feet in meters
+let result2=60*40/10.765;
+console.log(result2+" meter");
+
+//Calculate area of 25 such plots in acres
+
+let result3=25*0.05510450007;
+console.log("Acres : "+result3);
